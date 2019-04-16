343bf576cc734f587fec57d69bf46fd15774629a
Mathew Witek <matwitek1997@gmail.com>
2019-04-16 16:04:33 -0400

    Add files via upload

---
+++ Peer Review Assignment 3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Peer Asst 3
+//
+//  Created by Mathew on 5/27/18.
+//  Copyright © 2018 Mathew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Peer Review Assignment 3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Peer Asst 3
+//
+//  Created by Mathew on 5/27/18.
+//  Copyright © 2018 Mathew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
