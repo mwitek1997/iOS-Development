a226b9924cc8708e3cd8f84508096171bdd9b4f6
Mathew Witek <matwitek1997@gmail.com>
2019-04-16 16:08:08 -0400

    Add files via upload

---
+++ Peer Review Assignment 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Peer Review Asst2
+//
+//  Created by Mathew on 5/14/18.
+//  Copyright © 2018 Mathew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Peer Review Assignment 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Peer Review Asst2
+//
+//  Created by Mathew on 5/14/18.
+//  Copyright © 2018 Mathew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
